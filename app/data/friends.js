b64c4a3b0146fe82c881fa8c93ec41d143be67fd
Ryan Mendoza <ryanmendoza925@gmail.com>
2017-10-04 20:15:29 -0400

    Finished survey frontend

---
+++ app/data/friends.js
@@ -0,0 +1,5 @@
+function friends(name, photo, scores) {
+	this.name = name;
+	this.photo = photo;
+	this.scores = scores;
+}
--- server.js
+++ server.js
@@ -1,3 +1,25 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", function (req, res){
+	res.sendFile(path.join(__dirname, "/app/public/home.html"));
+});
+
+app.get("/survey", function (req, res){
+	res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+});
+
+app.get("/api/friends", function (req, res){
+	
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
