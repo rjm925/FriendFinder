5935193e846d18193e8a4c5cb431818468e22393
Ryan Mendoza <ryanmendoza925@gmail.com>
2017-10-09 16:59:10 -0400

    Added initial photos

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friends = [
 	{
 		name: "Bob",
-		photo: "http://via.placeholder.com/500x150",
+		photo: "https://s7d2.scene7.com/is/image/PetSmart/PB1201_STORY_CARO-Authority-HealthyOutside-DOG-20160818?$PB1201$",
 		scores: ["1", "3", "5", "4", "1", "2", "5", "4", "1", "3"]
 	},
 	{
 		name: "John",
-		photo: "http://via.placeholder.com/500x200",
+		photo: "http://www.readersdigest.ca/wp-content/uploads/2011/01/4-ways-cheer-up-depressed-cat.jpg",
 		scores: ["4", "2", "2", "1", "5", "3", "1", "5", "3", "4"]
 	},
 	{
 		name: "Jane",
-		photo: "http://via.placeholder.com/500x550",
+		photo: "https://www.petakids.com/wp-content/uploads/2015/11/Cute-Red-Bunny.jpg",
 		scores: ["2", "5", "1", "3", "2", "5", "3", "2", "4", "2"]
 	},
 	{
 		name: "Tom",
-		photo: "http://via.placeholder.com/500x450",
+		photo: "https://qph.ec.quoracdn.net/main-qimg-79826e3824e02fd1a92294f5b066873a",
 		scores: ["3", "1", "4", "5", "3", "1", "4", "1", "2", "5"]
 	},
 	{
 		name: "Justin",
-		photo: "http://via.placeholder.com/500x650",
+		photo: "http://woodstream.scene7.com/is/image/woodstream/hh-animals-groundhog-6?$ProductPgLarge2$",
 		scores: ["5", "4", "3", "2", "4", "4", "2", "3", "5", "1"]
 	}
 ];
