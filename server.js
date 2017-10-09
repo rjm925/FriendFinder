3e26345e4e021ddece2ad4866f23bdbd4be2a7eb
Ryan Mendoza <ryanmendoza925@gmail.com>
2017-10-09 14:09:43 -0400

    Heroku modifications

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = 3001;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
