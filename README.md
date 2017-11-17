fce067c2c6f3be2a01f57ad42c58cc647f860a12
Ryan Mendoza <ryanmendoza925@gmail.com>
2017-11-17 17:56:51 -0500

    commented and readme updated

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# FriendFinder
+# Friend Finder
+
+[Link to App](https://obscure-peak-93122.herokuapp.com/)
+
+This full-stack site will take in results from your users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match.
+
+# Technology Used
+* Node.js
+* Express
+* Heroku
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,28 @@
+// Array of friends info
 var friends = [
 	{
-		name: "Bob",
-		photo: "https://s7d2.scene7.com/is/image/PetSmart/PB1201_STORY_CARO-Authority-HealthyOutside-DOG-20160818?$PB1201$",
+		name: "Julia",
+		photo: "https://i.pinimg.com/736x/f5/a0/62/f5a0626a80fe6026c0ac65cdc2d8ede2--photography-portraits-photography-people.jpg",
 		scores: ["1", "3", "5", "4", "1", "2", "5", "4", "1", "3"]
 	},
 	{
 		name: "John",
-		photo: "http://www.readersdigest.ca/wp-content/uploads/2011/01/4-ways-cheer-up-depressed-cat.jpg",
+		photo: "https://drfancy.files.wordpress.com/2011/09/portraits61.jpg",
 		scores: ["4", "2", "2", "1", "5", "3", "1", "5", "3", "4"]
 	},
 	{
 		name: "Jane",
-		photo: "https://www.petakids.com/wp-content/uploads/2015/11/Cute-Red-Bunny.jpg",
+		photo: "https://i.pinimg.com/736x/83/ef/d2/83efd2f83d050642e5de3adbbfcfd5bc--black-white-fashion-fashion-portraits.jpg",
 		scores: ["2", "5", "1", "3", "2", "5", "3", "2", "4", "2"]
 	},
 	{
-		name: "Tom",
-		photo: "https://qph.ec.quoracdn.net/main-qimg-79826e3824e02fd1a92294f5b066873a",
+		name: "Julie",
+		photo: "https://i.ytimg.com/vi/TZiQK81Rjfw/maxresdefault.jpg",
 		scores: ["3", "1", "4", "5", "3", "1", "4", "1", "2", "5"]
 	},
 	{
 		name: "Justin",
-		photo: "http://woodstream.scene7.com/is/image/woodstream/hh-animals-groundhog-6?$ProductPgLarge2$",
+		photo: "https://i.pinimg.com/originals/39/e9/b3/39e9b39628e745a39f900dc14ee4d9a7.jpg",
 		scores: ["5", "4", "3", "2", "4", "4", "2", "3", "5", "1"]
 	}
 ];
